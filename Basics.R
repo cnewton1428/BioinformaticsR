7f0d1ffb2913d7ef2466d1e4f3eaa316370d4997
cnewton1428 <43345025+cnewton1428@users.noreply.github.com>
2021-01-06 14:09:43 -0500

    Add files via upload

---
+++ Basics.R
@@ -0,0 +1,30 @@
+# R Test Script
+# Carina Newton
+# October 18, 2019
+# The purpose of this script is to learn how to work in R
+
+count <- 0
+primes <- c(1, 3, 5, 7, 11)
+Names <- c("Bob", "Ted", "Carol", "Alice")
+Truth <- c(TRUE, TRUE)
+
+organism <- c("Human", "Chimpanzee", "Yeast")
+chromosomes <- c(23, 24, 16)
+multicellular <- c(TRUE, TRUE, FALSE)
+
+OrganismTable <- data.frame(organism, chromosomes, multicellular)
+
+write.table(OrganismTable, file = "MyData.csv",row.names=FALSE, na="",col.names=FALSE, sep=",")
+
+NewDataFrame <- read.csv("MyData.csv", header=TRUE, sep=",")
+
+count <- 0
+for (val in OrganismTable$chromosomes) {
+  if (val > 20) count = count+1
+}
+print(count)
+
+barplot(OrganismTable$chromosomes)
+
+
+
